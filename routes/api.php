939d729f02d1bbdca57431c81f8a4a47426ff44e
Uchedivine <uchedivine65@gmail.com>
2025-10-22 01:35:57 +0100

    chore: finalize API fixes, add RouteServiceProvider and migrations

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\StringController;
 
 Route::prefix('strings')->group(function () {
+    Route::post('/', [StringController::class, 'store']); 
     Route::get('/', [StringController::class, 'index']);
-    Route::get('/filter-by-natural-language', [StringController::class, 'naturalFilter']); // Add this line
-    Route::post('/', [StringController::class, 'store']);
+    Route::get('/filter-by-natural-language', [StringController::class, 'naturalFilter']);
     Route::get('/{value}', [StringController::class, 'show']);
     Route::delete('/{value}', [StringController::class, 'destroy']);
 });
